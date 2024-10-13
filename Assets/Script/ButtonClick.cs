46e7874b4c7d99b2d1b4956a0e4a0ecd4d67f302
minji3821 <m2477@naver.com>
2024-10-14 02:07:51 +0900

    go

---
+++ Assets/Script/ButtonClick.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonClick : MonoBehaviour
+{
+    public void HiWhite()
+    {
+        Debug.Log("안녕 난 하얀색 버튼이야!");
+    }
+
+    public void HiSkyblue()
+    {
+        Debug.Log("안녕 난 하늘색 버튼이야!");
+    }
+}
